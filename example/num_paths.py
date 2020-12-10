36a78cc6c4b81890e6c7706b1cc5dbc370b57f96
SenselessMite <pasqualecavoto1996@gmail.com>
2020-12-10 19:12:41 +0100

    Removed example folder

--- example/num_paths.py
+++
@@ -1,5 +0,0 @@
-
-def find_num_paths(right_path,left_path):
-    if(right_path==0)or(left_path==0):
-        return 1
-        return find_num_paths(right_path -1, left_path)+ find_num_paths(right_path,left_path -1)
