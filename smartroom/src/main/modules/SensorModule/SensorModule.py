ffcc6195b6ac8ec48be5ecbf8cccc49d97b8a5cc
SenselessMite <pasqualecavoto1996@gmail.com>
2020-12-19 12:59:21 +0100

    modified SensorModule.py

--- smartroom/src/main/modules/SensorModule/SensorModule.py
+++ smartroom/src/main/modules/SensorModule/SensorModule.py
@@ -1,21 +1,11 @@
 class SensorModule:
-	ref_value = 0
-	curr_value = 0 
-
-	def _init_(self, ref_value = 0):
-		self.ref_value = ref_value
-
-	def setRefValue(ref_value):
-		self.ref_value = ref_value
-
-	def getRefValue():
-        	return self.ref_value
+	curr_value = 0
 
 	def setCurrValue(curr_value):
 		self.curr_value = curr_value
 
 	def getCurrValue():
 		return self.curr_value
-	
+
 	def startMeasure():
 		return 0
