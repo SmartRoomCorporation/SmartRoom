206c5a719e690f7d3cc5a649bb638dda9e8cc350
SenselessMite <pasqualecavoto1996@gmail.com>
2020-12-19 11:08:41 +0000

    modfied temperature module source code

--- modules/SensorModule/SensorModule.py
+++ modules/SensorModule/SensorModule.py
@@ -1,20 +1,21 @@
 class SensorModule:
-    ref_value = 0
-    curr_value = 0
-    
+	ref_value = 0
+	curr_value = 0 
 
-    def _init_(self, ref_value = 0):
-        self.ref_value = ref_value
+	def _init_(self, ref_value = 0):
+		self.ref_value = ref_value
 
-    def setRefValue(ref_value):
-        self.ref_value = ref_value
+	def setRefValue(ref_value):
+		self.ref_value = ref_value
 
-    def getRefValue():
-        return self.ref_value
+	def getRefValue():
+        	return self.ref_value
 
-    def getCurrValue():
-        return self.curr_value
+	def setCurrValue(curr_value):
+		self.curr_value = curr_value
 
-    def startSensorControl():
-        #implements sensor control
-
+	def getCurrValue():
+		return self.curr_value
+	
+	def startMeasure():
+		return 0
--- modules/SensorModule/TempModule/TempModule.py
+++ modules/SensorModule/TempModule/TempModule.py
@@ -1,2 +1,14 @@
+import time
+import board
+import adafruit_dht as ad
+
 class TempModule(SensorModule):
     #This module implements the temperature control function
+	data_pin = 0
+	def _init_(self, ref_value, data_pin)
+		super()._init_(self, ref_value)
+		self.data_pin = 
+
+	def startMeasure(self):
+		try:
+			temp = 
