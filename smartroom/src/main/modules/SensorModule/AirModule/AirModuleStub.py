3e27efc0d2d42dc6839ea6ab59eac58ed332ee53
SenselessMite <pasqualecavoto1996@gmail.com>
2020-12-20 18:50:24 +0100

    Implemented simple stub SensorModule to test code

--- smartroom/src/main/modules/SensorModule/AirModule/AirModuleStub.py
+++ smartroom/src/main/modules/SensorModule/AirModule/AirModuleStub.py
@@ -1,27 +1,27 @@
 from .. import SensorModule
 
-class TempModuleStub(SensorModule.SensorModule):
+class AirModuleStub(SensorModule.SensorModule):
     #This module implements the temperature control function
 	count = 0
 
 	def startMeasure(self):
 		if self.count == 0:
 			self.count = self.count + 1
-			return self.setCurrValue(20)
+			return self.setCurrValue(80)
 
 		if self.count > 0 and self.count < 20:
 			if (self.count % 3) == 0:
-				if self.getCurrValue() > 10:
-					self.setCurrValue(self.getCurrValue() - 1)
-				else:
+				if self.getCurrValue() > 50:
+					self.setCurrValue(self.getCurrValue() - 2)
+                else:
                     self.setCurrValue(self.getCurrValue() + 1)
 
 		if self.count > 30 and self.count < 60:
 			if (self.count % 3) == 0:
-				if self.getCurrValue() < 28:
-					self.setCurrValue(self.getCurrValue() + 1)
+				if self.getCurrValue() < 100:
+					self.setCurrValue(self.getCurrValue() + 5)
 				else:
-					self.setCurrValue(self.getCurrValue() - 1)
+					self.setCurrValue(self.getCurrValue() - 3)
 
 		if self.count > 70:
 			self.count = 0
--- smartroom/src/main/modules/SensorModule/LightModule/LightModuleStub.py
+++ smartroom/src/main/modules/SensorModule/LightModule/LightModuleStub.py
@@ -1,27 +1,27 @@
 from .. import SensorModule
 
-class TempModuleStub(SensorModule.SensorModule):
+class LightModuleStub(SensorModule.SensorModule):
     #This module implements the temperature control function
 	count = 0
 
 	def startMeasure(self):
 		if self.count == 0:
 			self.count = self.count + 1
-			return self.setCurrValue(20)
+			return self.setCurrValue(1500)
 
 		if self.count > 0 and self.count < 20:
 			if (self.count % 3) == 0:
-				if self.getCurrValue() > 10:
-					self.setCurrValue(self.getCurrValue() - 1)
-				else:
-                    self.setCurrValue(self.getCurrValue() + 1)
+				if self.getCurrValue() > 1000:
+					self.setCurrValue(self.getCurrValue() - 150)
+                else:
+                    self.setCurrValue(self.getCurrValue() + 80)
 
 		if self.count > 30 and self.count < 60:
 			if (self.count % 3) == 0:
-				if self.getCurrValue() < 28:
-					self.setCurrValue(self.getCurrValue() + 1)
+				if self.getCurrValue() < 2500:
+					self.setCurrValue(self.getCurrValue() + 150)
 				else:
-					self.setCurrValue(self.getCurrValue() - 1)
+					self.setCurrValue(self.getCurrValue() - 100)
 
 		if self.count > 70:
 			self.count = 0
