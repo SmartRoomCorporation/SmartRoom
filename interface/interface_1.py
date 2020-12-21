6c4c404fe4292f15bc2c070c7b78308c7c28ee9b
augusto ungolo <augustoungolo@Air-di-Augusto.homenet.telecomitalia.it>
2020-12-21 17:12:49 +0100

    modified script interface

--- interface/interface_1.py
+++ interface/interface_1.py
@@ -11,6 +11,10 @@ up=Image.open("up.png")
 up=up.resize((20,20))
 up_icon=ImageTk.PhotoImage(up)
 
+up1=Image.open("down.png")
+up1=up1.resize((20,20))
+up_icon1=ImageTk.PhotoImage(up1)
+
 #andiamo a personalizzarla dandole un'altezza e larghezza
 
 window.geometry("900x700")
@@ -23,7 +27,7 @@ window.configure(bg="grey")
 #ora lo posizioniamo nella nostra schermata
 
 welcome_label = tk.Label(window, text="Welcome in the My SmartRoom", font=("Helvetica",15))
-welcome_label.grid(row=0, column=0, sticky="WE",padx=20,pady=10)
+welcome_label.grid(row=0, column=0, sticky="WE",padx=70,pady=10)
 
 welcome_label = tk.Label(window, text="Lista Sensori", font=("Helvetica",15))
 welcome_label.grid(row=1, column=0, sticky="WE",padx=20,pady=10)
@@ -33,17 +37,17 @@ text = "Sensore 1:"
 text_output = tk.Label(window, text=text, fg="Green", font=("Helevetica",16))
 text_output.grid(row=2, column=0,sticky="W")
 text = "valore:"
-text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",16))
+text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",13))
 text_output.grid(row=3, column=0,pady="5",sticky="W")
 text = "Trigger:"
-text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",16))
+text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",13))
 text_output.grid(row=4, column=0,pady="5",sticky="W")
-first_button = tk.Button(text="Aumenta")
+first_button = tk.Button(image=up_icon)
 first_button.grid(row=4, column=1,padx="30",sticky="W")
-first_button = tk.Button(text="Diminuisci")
+first_button = tk.Button(image=up_icon1)
 first_button.grid(row=4, column=1,padx="100",sticky="W")
 text = "Stato: "
-text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",16))
+text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",13))
 text_output.grid(row=5, column=0,pady="5",sticky="W")
 
 
@@ -52,34 +56,34 @@ text = "Sensore 2:"
 text_output = tk.Label(window, text=text, fg="Green", font=("Helevetica",16))
 text_output.grid(row=6, column=0,sticky="W")
 text = "valore:"
-text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",16))
+text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",13))
 text_output.grid(row=7, column=0,pady="5",sticky="W")
 text = "Trigger:"
-text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",16))
+text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",13))
 text_output.grid(row=8, column=0,pady="5",sticky="W")
-first_button = tk.Button(text="Aumenta")
+first_button = tk.Button(image=up_icon)
 first_button.grid(row=8, column=1,padx="30",sticky="W")
-first_button = tk.Button(text="Diminuisci")
+first_button = tk.Button(image=up_icon1)
 first_button.grid(row=8, column=1,padx="100",sticky="W")
 text = "Stato: "
-text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",16))
+text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",13))
 text_output.grid(row=9, column=0,pady="5",sticky="W")
 
 text = "Sensore 3:"
 text_output = tk.Label(window, text=text, fg="Green", font=("Helevetica",16))
 text_output.grid(row=10, column=0,sticky="W")
 text = "valore:"
-text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",16))
+text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",13))
 text_output.grid(row=11, column=0,pady="5",sticky="W")
 text = "Trigger:"
-text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",16))
+text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",13))
 text_output.grid(row=12, column=0,pady="5",sticky="W")
-first_button = tk.Button(text="Aumenta")
+first_button = tk.Button(image=up_icon1)
 first_button.grid(row=12, column=1,padx="30",sticky="W")
-first_button = tk.Button(text="Diminuisci")
+first_button = tk.Button(image=up_icon1)
 first_button.grid(row=12, column=1,padx="100",sticky="W")
 text = "Stato: "
-text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",16))
+text_output = tk.Label(window, text=text, fg="black", font=("Helevetica",13))
 text_output.grid(row=13, column=0,pady="5",sticky="W")
 
 
