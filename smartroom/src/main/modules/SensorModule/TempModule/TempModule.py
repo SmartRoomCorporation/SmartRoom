9e882644c7760969a8943b843c3d396b1999bc50
augusto ungolo <augustoungolo@Air-di-Augusto.homenet.telecomitalia.it>
2020-12-23 12:13:34 +0100

    modified TempModule

--- smartroom/src/main/modules/SensorModule/TempModule/TempModule.py
+++ smartroom/src/main/modules/SensorModule/TempModule/TempModule.py
@@ -1,6 +1,10 @@
 import time
 import board
 import adafruit_dht as ad
+from PIL import Image, ImageTk
+import tkinter as tk
+
+
 
 class TempModule(SensorModule):
     #This module implements the temperature control function
@@ -15,3 +19,32 @@ class TempModule(SensorModule):
 			return super().getCurrValue()
 		except Exception as Error:
 			return super().getCurrValue()
+
+
+
+
+class GuiSensorTemp():
+	
+	def CreateTempGui(self, block):
+		up=Image.open("up.png")
+   	 	up=up.resize((20,20))
+    	up_icon=ImageTk.PhotoImage(up)
+    	up1=Image.open("down.png")
+    	up1=up1.resize((20,20))
+    	up_icon1=ImageTk.PhotoImage(up1)
+		text = "Sensore 1:"
+		text_output = tk.Label(block, text=text, fg="Green", font=("Helevetica",16))
+		text_output.grid(row=2, column=0,sticky="W")
+		text = "valore:"
+		text_output = tk.Label(block, text=text, fg="black", font=("Helevetica",13))
+		text_output.grid(row=3, column=0,pady="5",sticky="W")
+		text = "Trigger:"
+		text_output = tk.Label(block, text=text, fg="black", font=("Helevetica",13))
+		text_output.grid(row=4, column=0,pady="5",sticky="W")
+		first_button = tk.Button(image=up_icon)
+		first_button.grid(row=4, column=1,padx="30",sticky="W")
+		first_button = tk.Button(image=up_icon1)
+		first_button.grid(row=4, column=1,padx="100",sticky="W")
+		text = "Stato: "
+		text_output = tk.Label(block, text=text, fg="black", font=("Helevetica",13))
+		text_output.grid(row=5, column=0,pady="5",sticky="W")	
