fa8ce087569c4040647270a80b59bbca7aab106a
SenselessMite <pasqualecavoto1996@gmail.com>
2020-12-29 16:07:19 +0100

    Fixed gui layout

--- server/ServerGui/ServerConfig.py
+++ server/ServerGui/ServerConfig.py
@@ -6,17 +6,45 @@ class ServerConfig(tk.Toplevel):
     def __init__(self, master = None):
         self.master = master
         super().__init__(master = master)
-        self.geometry("200x300")
+        self.geometry("420x190")
+        self.resizable(width=False, height=False)
         self.protocol("WM_DELETE_WINDOW", self.onClose)
+        self.title("Connection Setting")
         ip_frame = tk.LabelFrame(self, text = "IP:", fg = "blue")
-        ip_frame.pack(fill = tk.X)
+        ip_frame.grid(row = 0, sticky = "nsew", padx = 10, pady = 10)
         lab = tk.Label(ip_frame, text = "Current IP:")
-        lab.grid(column = 0, columnspan = 1, row = 0, padx = 5, pady = 5, sticky = tk.W)
-        ip_frame.grid_columnconfigure(0, weight=0)
+        lab.pack(side = LEFT, padx = 5, pady = 5)
+
         cur_ip = tk.Label(ip_frame, text = "192.168.0.111")
-        cur_ip.grid(column = 1, columnspan = 1, row = 0, padx = 5, pady = 5)
-        ip_text = tk.Text(ip_frame, height=1, width=20)
-        ip_text.grid(column = 0, row = 1, padx = 5, pady = 5, sticky = tk.W)
+        cur_ip.pack(side = LEFT, padx = 5, pady = 5)
+
+        tk.Frame(ip_frame, width = 50).pack(side = LEFT)
+        lab1 = tk.Label(ip_frame, text = "New IP:")
+        lab1.pack(side = LEFT, padx = 5, pady = 5)
+
+        self.ip_entry = tk.Entry(ip_frame, width=20)
+        self.ip_entry.pack(side = LEFT, padx = 5, pady = 5)
+
+        port_frame = tk.LabelFrame(self, text = "PORT:", fg = "blue")
+        port_frame.grid(row = 1, sticky = "nsew", padx = 10, pady = 10)
+        lab2 = tk.Label(port_frame, text = "Current PORT:")
+        lab2.pack(side = LEFT, padx = 5, pady = 5)
+
+        cur_port = tk.Label(port_frame, text = "1883")
+        cur_port.pack(side = LEFT, padx = 5, pady = 5)
+
+        tk.Frame(port_frame, width = 75).pack(side = LEFT)
+        lab3 = tk.Label(port_frame, text = "New PORT:")
+        lab3.pack(side = LEFT, padx = 5, pady = 5)
+
+        self.port_entry = tk.Entry(port_frame, width=10)
+        self.port_entry.pack(side = LEFT, padx = 5, pady = 5)
+
+        back_button = tk.Button(self, text = "Back", command = self.onClose)
+        back_button.grid(row = 2, sticky = "sw", padx = 10, pady = 10)
+
+        save_button = tk.Button(self, text = "Save")
+        save_button.grid(row = 2, sticky = "se", padx = 10, pady = 10)
 
     def onClose(self):
         self.master.wm_attributes("-disabled", False)
--- server/ServerGui/ServerGui.py
+++ server/ServerGui/ServerGui.py
@@ -8,9 +8,12 @@ from ServerConfig import ServerConfig
 class ServerGui:
     window = tk.Tk()
     window.geometry("600x600")
-    window.title("SmartRooms Control Server")
+    window.minsize(520, 600)
+    window.title("SmartRooms Control Utility")
     set_icon = None
-    
+    curr_ip = "0.0.0.0"
+    curr_port = "0000"
+
     def __init__(self):
         set_img = Image.open("setting.png")
         set_img = set_img.resize((20, 20))
@@ -23,26 +26,38 @@ class ServerGui:
 
     def initGui(self):
         self.statusFrame = tk.LabelFrame(self.window, text = "Server Status:", fg="blue")
-        self.statusFrame.pack(fill = tk.BOTH)
+        self.statusFrame.pack(fill = tk.BOTH, padx = 10, pady = 10)
 
-        lab = tk.Label(self.statusFrame, text = "Connection status:")
-        lab.grid(column = 0, row = 0, padx = 5, pady = 5)
+        frame_cs = tk.Frame(self.statusFrame)
+        frame_cs.grid(row = 0, column = 0)
+        lab = tk.Label(frame_cs, text = "Connection status:")
+        lab.pack(side = tk.LEFT,  padx= 5, pady = 5)
+        sc = StatusCircle(frame_cs, height = 50, width = 50)
+        sc.pack(side = tk.LEFT,  padx= 5, pady = 5)
 
-        sc = StatusCircle(self.statusFrame, height = 50, width = 50)
-        sc.grid(column = 2, row = 0, padx = 5, pady = 5)
+        frame_ip = tk.Frame(self.statusFrame)
+        frame_ip.grid(row = 0, column = 1)
+        lab1 = tk.Label(frame_ip, text = "IP:")
+        lab1.pack(side = tk.LEFT,  padx= 5, pady = 5)
+        text1 = tk.StringVar(frame_ip, self.curr_ip)
+        ip_lab = tk.Label(frame_ip, textvariable = text1)
+        ip_lab.pack(side = tk.LEFT,  padx= 5, pady = 5)
 
-        lab1 = tk.Label(self.statusFrame, text = "IP:")
-        lab1.grid(column = 0, row = 2, padx= 5, pady = 5, sticky = tk.W)
-        lab2 = tk.Label(self.statusFrame, text = "Port:")
+        frame_port = tk.Frame(self.statusFrame)
+        frame_port.grid(row = 0, column = 2)
+        lab2 = tk.Label(frame_port, text = "Port:")
+        lab2.pack(side = tk.LEFT,  padx= 5, pady = 5)
+        text2 = tk.StringVar(frame_port, self.curr_port)
+        port_lab = tk.Label(frame_port, textvariable = text2)
+        port_lab.pack(side = tk.LEFT,  padx= 5, pady = 5)
 
-        lab2.grid(column = 1, row = 2, padx = 5, pady = 5)
         self.statusFrame.grid_columnconfigure(3, weight=1)
-
+        
         configButton = tk.Button(self.statusFrame, image = self.set_icon, command = self.configWindow)
-        configButton.grid(column = 3, row = 0, padx = 5, pady = 5, sticky = tk.E)
+        configButton.grid(row = 0, column = 3, sticky = "se",  padx= 5, pady = 5)
 
         statusCard = ServerCard(self.window)
-        statusCard.pack(expand=1, fill = tk.BOTH)
+        statusCard.pack(expand=1, fill = tk.BOTH, padx = 10, pady = 10)
 
         statusCard.initCard()
 
