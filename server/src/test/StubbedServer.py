eef3aad047a10cff6ca8e87f57bdb374525d7ba7
Matteo Bosco <matteobosco@alice.it>
2021-01-02 15:08:41 +0100

    fix

--- server/src/test/StubbedServer.py
+++ server/src/test/StubbedServer.py
@@ -1,42 +1,54 @@
-import paho.mqtt.client as mqtt
+iimport paho.mqtt.client as mqtt
 from threading import Thread
 from uuid import getnode as get_mac
 import json
 
-server = mqtt.Client()
-clientTopic = ':'.join(("%012X" % get_mac())[i:i+2] for i in range(0, 12, 2))
-serverTopic = ':'.join(("%012X" % get_mac())[i:i+2] for i in range(0, 12, 2))+'-server' # output from client
+class StubbedServer():
 
-def on_connect(client, userdata, flags, rc): # on connect callback
-    client.subscribe("subreqq")
-    client.subscribe(serverTopic)
+    clientTopic = None
+    serverTopic = None
 
-def on_message(client, userdata, msg): # on message callback
-    print(str(msg.payload.decode("utf-8")))
-    if(str(msg.payload.decode("utf-8")) == "TESTAUTOPILOT"):
-        data = ["Temperature", "AUTOOFF"]
-        message = ["COMMAND", data]
-        client.publish(clientTopic, json.dumps(message), qos=0, retain=False)
-    elif(str(msg.payload.decode("utf-8")) == "TESTRISE"):
-        data = ["Air", "RISE"]
-        message = ["COMMAND", data]
-        client.publish(clientTopic, json.dumps(message), qos=0, retain=False)
-    elif(str(msg.payload.decode("utf-8")) == "TESTREDUCE"):
-        data = ["Light", "REDUCE"]
-        message = ["COMMAND", data]
-        client.publish(clientTopic, json.dumps(message), qos=0, retain=False)
-    elif(str(msg.payload.decode("utf-8")) == "TESTCOMMAND"):
-        data = ["Air", "ACTUATOR", "FANUP"]
-        message = ["COMMAND", data]
-        client.publish(clientTopic, json.dumps(message), qos=0, retain=False)
-    elif(str(msg.payload.decode("utf-8")) == "STOP"):
-        client.loop_stop()
-        client.disconnect()
+    def on_connect(self, client, userdata, flags, rc): # on connect callback
+        client.subscribe("subreqq")
 
+    def setTopic(self, topic):
+        self.clientTopic = topic
+        self.serverTopic = topic + '-server'
+        self.server.subscribe(self.serverTopic)
 
-server.on_message = on_message
-server.on_connect = on_connect
+    def getClientTopic(self):
+        return self.clientTopic
 
+    def on_message(self, client, userdata, msg): # on message callback
+        print(str(msg.payload.decode("utf-8")))
+        clientTopic = getClientTopic()
+        print(clientTopic)
+        if(msg.topic == "subreqq"): setTopic(str(msg.payload.decode("utf-8")))
+        if(str(msg.payload.decode("utf-8")) == "TESTAUTOPILOT"):
+            data = ["Temperature", "AUTOOFF"]
+            message = ["COMMAND", data]
+            client.publish(clientTopic, json.dumps(message), qos=0, retain=False)
+        elif(str(msg.payload.decode("utf-8")) == "TESTRISE"):
+            data = ["Air", "RISE"]
+            message = ["COMMAND", data]
+            client.publish(clientTopic, json.dumps(message), qos=0, retain=False)
+        elif(str(msg.payload.decode("utf-8")) == "TESTREDUCE"):
+            data = ["Light", "REDUCE"]
+            message = ["COMMAND", data]
+            client.publish(clientTopic, json.dumps(message), qos=0, retain=False)
+        elif(str(msg.payload.decode("utf-8")) == "TESTCOMMAND"):
+            data = ["Air", "ACTUATOR", "FANUP"]
+            message = ["COMMAND", data]
+            client.publish(clientTopic, json.dumps(message), qos=0, retain=False)
+        elif(str(msg.payload.decode("utf-8")) == "STOP"):
+            client.loop_stop()
+            client.disconnect()
 
-server.connect("127.0.0.1", 1883, 60)
-server.loop_forever()
+    def __init__(self):
+        self.server = mqtt.Client()
+        self.server.on_message = self.on_message
+        self.server.on_connect = self.on_connect
+        self.server.connect("87.7.152.200", 1883, 60)
+        self.server.loop_forever()
+
+s = StubbedServer()
