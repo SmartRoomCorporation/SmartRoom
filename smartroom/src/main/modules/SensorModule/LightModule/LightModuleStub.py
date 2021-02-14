370dc5bfd794bdb9db2c7b07197f66e67e1b8b5e
Matteo Bosco <matteobosco@alice.it>
2021-02-14 20:51:23 +0100

    fix

--- smartroom/src/main/modules/SensorModule/LightModule/LightModuleStub.py
+++ smartroom/src/main/modules/SensorModule/LightModule/LightModuleStub.py
@@ -50,14 +50,14 @@ class LightModuleStub(SensorModule.SensorModule):
 
 	def startMeasure(self):
 		if self.count == 0:
-			self.setCurrValue(3000)
+			self.setCurrValue(1500)
 		if self.count > 0 and self.count < 20:
 			if (self.count % 3) == 0:
-				if self.getCurrValue() > 3000: self.setCurrValue(self.getCurrValue() - 150)
+				if self.getCurrValue() > 1000: self.setCurrValue(self.getCurrValue() - 150)
 				else: self.setCurrValue(self.getCurrValue() + 80)
 		if self.count > 30 and self.count < 60:
 			if (self.count % 3) == 0:
-				if self.getCurrValue() < 2600:
+				if self.getCurrValue() < 2500:
 					self.setCurrValue(self.getCurrValue() + 150)
 				else:
 					self.setCurrValue(self.getCurrValue() - 100)
