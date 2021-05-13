4de39816dbb06e3d2f06f2c711762a5a6d44080d
Matteo Bosco <matteobosco@alice.it>
2021-05-13 11:19:58 +0200

    IP

--- server/src/main/Main.py
+++ server/src/main/Main.py
@@ -2,7 +2,7 @@ from Server import SmartroomServer
 from ServerGui.ServerGui import ServerGui
 
 server = SmartroomServer()
-server.setIp("95.239.24.91")
+server.setIp("79.43.58.243")
 server.start()
 gui = ServerGui(server)
 gui.run()
